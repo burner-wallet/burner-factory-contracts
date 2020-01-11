66e1541b04caab679c53298e923c594e306ac9aa
David Mihal <david@dmihal.com>
2020-01-11 15:54:09 -0500

    Add rinkeby to truffle

--- truffle-config.js
+++ truffle-config.js
@@ -55,6 +55,15 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+      network_id: 4,       // Ropsten's id
+      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
